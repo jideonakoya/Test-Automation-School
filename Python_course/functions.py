e4796e7960fd6b9c37264e0d99906ca09cf48d1d
Jide Onakoya <jdonakoya@gmail.com>
2022-10-04 20:29:58 +0100

    Updated with function task

---
+++ Python_course/functions.py
@@ -0,0 +1,40 @@
+print("function\n")
+
+
+def greet():
+    print("Hello world from python")
+
+
+def goodbye():
+    print("Thank You")
+    print("Good bye")
+
+
+greet()
+goodbye()
+
+print("continuation\n")
+
+
+def add(num1, num2):
+    result = num1 + num2
+    print(result)
+
+
+def subtract(num1, num2):
+    result = num1 - num2
+    print(result)
+
+
+def multiply(num1, num2, num3):
+    result = num1 * num2 * num3
+    print(result)
+
+
+add(4, 3)
+subtract(7, 2)
+multiply(3, 4, 5)
+
+
+def validate_username():
+    pass
