f44a21014827aef396dc7f8cc6e083c01de92cce
Jide Onakoya <jdonakoya@gmail.com>
2022-10-07 15:30:08 +0100

    tasks completion

--- Python_course/Task16.py
+++ Python_course/Task16.py
@@ -1,21 +1,11 @@
-car_parts = {
-    "model": "Benz",
-    "engineNo": "GHW123",
-    "ChasisNo": "ert0987",
-    "Owner": "Adetomiwa",
-    "type": "automatic",
-    "gear": "4wheelDrive",
-    "ignition": "pushToStart"
+
+laptop = {
+    "make": "Lenovo",
+    "model": "Ideapad",
+    "model number": "LIB676",
+    "Colour": "Silver"
 }
 
-print(car_parts)
-print(car_parts.get("model")) #to get the value of a key
-#print(car_parts.items()) #to turn it to a list of tuples
-print(car_parts.keys()) #to return the keys in the dictionary
-print(car_parts.values()) #to return the values in the dictionary
-car_parts.pop("gear") #to remove a key from a dictionary
-print(car_parts)
-car_parts.update({"gear": "4wheelDrive", "color": "red"})
-print(car_parts)
-print(car_parts.popitem()) #to print out the last key in the dictionary, the original dictionary would not contain the key removed.
-print(car_parts)
+print(laptop)
+laptop.update({"Keyboard colour": "Black", "Webcam": "Hi-Def"})
+print(laptop)
--- Python_course/basic_dictionary_ops.py
+++ Python_course/basic_dictionary_ops.py
@@ -1,12 +1,2 @@
 
 
-laptop = {
-    "make": "Lenovo",
-    "model": "Ideapad",
-    "model number": "LIB676",
-    "Colour": "Silver"
-}
-
-print(laptop)
-laptop.update({"Keyboard colour": "Black", "Webcam": "Hi-Def"})
-print(laptop)
